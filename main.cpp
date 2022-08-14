dac2dd11eaab015fc97e822c2718c981a99a62db
Ben Mindlin <bennm37@gmail.com>
2022-08-14 12:05:30 +0100

    Main,font and CMakeLists added

---
+++ main.cpp
@@ -0,0 +1,21 @@
+// #include "stdafx.h"
+#include "LorenzAttractor.h"
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+  /// Create a window
+
+  bool isFullscreen = true;
+  sf::RenderWindow window;
+    window.create(sf::VideoMode(2560,1600), "Coding Projects");
+	// window.create(sf::VideoMode(1920, 1000), "Coding Projects", (isFullscreen ? sf::Style::Fullscreen : sf::Style::Default), sf::ContextSettings());
+	window.setPosition(sf::Vector2i(0, 0));
+  window.setVerticalSyncEnabled(true); 
+	window.setFramerateLimit(60);
+  
+  
+//   /// Important part is here
+  LorenzAttractor la1;
+  la1.run(window);
+}
